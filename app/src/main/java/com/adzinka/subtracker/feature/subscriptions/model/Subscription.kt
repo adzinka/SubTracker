8b2e74a49ab99d272a3b542a8c7a4f6bc56ff90d
Yelizaveta Zhurava <yelizaveta.zhurava@gmail.com>
2026-03-08 23:39:33 +0100

    feat(data): add subscription data model

---
+++ app/src/main/java/com/adzinka/subtracker/feature/subscriptions/model/Subscription.kt
@@ -0,0 +1,20 @@
+package com.adzinka.subtracker.feature.subscriptions.model
+
+import androidx.compose.ui.graphics.Color
+
+data class Subscription(
+    val id: Int,
+    val name: String,
+    val category: String,
+    val nextPaymentDate: String,
+    val price: Int,
+    val currency: String = "CZK",
+    val period: String = "měsíčně",
+    val status: SubscriptionStatus = SubscriptionStatus.ACTIVE,
+    val iconEmoji: String,
+    val iconColor: Color
+)
+
+enum class SubscriptionStatus {
+    ACTIVE, PAUSED, SOON
+}
