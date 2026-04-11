0ba3ce64aed28ef0584c7e38063ca522e89fef69
Yelizaveta Zhurava <yelizaveta.zhurava@gmail.com>
2026-04-11 13:11:40 +0200

    feat(data): add payment dao

---
+++ app/src/main/java/com/adzinka/subtracker/data/PaymentDao.kt
@@ -0,0 +1,22 @@
+package com.adzinka.subtracker.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.adzinka.subtracker.data.entity.PaymentEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PaymentDao {
+    @Query("SELECT * FROM payments where subscriptionId = :subscriptionId")
+    fun getAllPaymentsBySubscriptionId(subscriptionId: Int): Flow<List<PaymentEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPayment(payment: PaymentEntity)
+
+    @Delete
+    suspend fun deletePayment(payment: PaymentEntity)
+}
+
