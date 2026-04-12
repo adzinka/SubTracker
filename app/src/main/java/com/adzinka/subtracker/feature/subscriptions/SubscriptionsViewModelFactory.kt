6d697d6f04e47c40e83d9f64abfe157e65ac8a2a
Yelizaveta Zhurava <yelizaveta.zhurava@gmail.com>
2026-04-12 18:07:14 +0200

    feat(vm): add subscription view model factory

---
+++ app/src/main/java/com/adzinka/subtracker/feature/subscriptions/SubscriptionsViewModelFactory.kt
@@ -0,0 +1,13 @@
+package com.adzinka.subtracker.feature.subscriptions
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.adzinka.subtracker.data.SubscriptionRepository
+
+class SubscriptionsViewModelFactory(
+    private val repository: SubscriptionRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return SubscriptionsViewModel(repository) as T
+    }
+}
