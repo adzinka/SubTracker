f02c80fbaea10e6e535539b17b1715b93c882bc7
Yelizaveta Zhurava <yelizaveta.zhurava@gmail.com>
2026-04-22 13:44:32 +0200

    fix(ui): allow LazyColumn to scroll to last item

--- app/src/main/java/com/adzinka/subtracker/feature/subscriptions/SubscriptionsScreen.kt
+++ app/src/main/java/com/adzinka/subtracker/feature/subscriptions/SubscriptionsScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -112,7 +113,14 @@ private fun SubscriptionsContent(
             Spacer(modifier = Modifier.height(8.dp))
 
             LazyColumn(
-                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                modifier = Modifier
+                    .weight(1f),
+                contentPadding = PaddingValues(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 8.dp,
+                    bottom = 88.dp
+                ),
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 items(
