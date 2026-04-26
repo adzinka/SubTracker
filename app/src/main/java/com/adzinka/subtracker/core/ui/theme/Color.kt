92d22b32d7dcee7da73ed44794528dadf32888fc
Yelizaveta Zhurava <yelizaveta.zhurava@gmail.com>
2026-04-26 20:19:23 +0200

    feat: add color tokens and theme setup

--- app/src/main/java/com/adzinka/subtracker/core/ui/theme/Color.kt
+++ app/src/main/java/com/adzinka/subtracker/core/ui/theme/Color.kt
@@ -16,12 +16,62 @@ object AppColors {
     val PurpleDark = Color(0xFF4A2FCC)
     val Background = Color(0xFFF5F5F5)
     val CardBackground = Color.White
-    val TextPrimary = Color(0xFF1A1A2E)
-    val TextSecondary = Color(0xFF888888)
+//    val TextPrimary = Color(0xFF1A1A2E)
+//    val TextSecondary = Color(0xFF888888)
     val BadgeSoon = Color(0xFFEDE8FF)
     val BadgeSoonText = Color(0xFF6B4EFF)
     val BadgePaused = Color(0xFFFFF3E0)
     val BadgePausedText = Color(0xFFFF9800)
 
     val White = Color(0xFFFFFFFF)
+
+
+    // region Brand
+    val Brand900 = Color(0xFF0F0E1A)
+    val Brand800 = Color(0xFF1E1B4B)
+    val Brand700 = Color(0xFF312E81)
+    // endregion
+
+    // region Accent (use sparingly)
+    val Accent500 = Color(0xFF6366F1)
+    val Accent100 = Color(0xFFE0E7FF)
+    // endregion
+
+    // region Categories
+    val CategoryStreaming = Color(0xFFDC2626)
+    val CategoryMusic = Color(0xFF059669)
+    val CategoryFitness = Color(0xFFEA580C)
+    val CategoryProductivity = Color(0xFF4F46E5)
+    val CategoryGaming = Color(0xFF7C3AED)
+    val CategoryCloud = Color(0xFF0891B2)
+    val CategoryNews = Color(0xFFCA8A04)
+    val CategoryEducation = Color(0xFF0D9488)
+    val CategoryOther = Color(0xFF6B7280)
+    // endregion
+
+    // region Detail screen hero (deeper category colors)
+    val HeroStreaming = Color(0xFF7F1D1D)
+    val HeroMusic = Color(0xFF064E3B)
+    val HeroFitness = Color(0xFF7C2D12)
+    val HeroProductivity = Color(0xFF312E81)
+    val HeroGaming = Color(0xFF4C1D95)
+    val HeroCloud = Color(0xFF164E63)
+    // endregion
+
+    // region Semantic
+    val Success = Color(0xFF10B981)
+    val Warning = Color(0xFFF59E0B)
+    val Danger = Color(0xFFEF4444)
+    val Info = Color(0xFF3B82F6)
+    // endregion
+
+    // region Neutrals
+    val BgPrimary = Color(0xFFFAFAFA)
+    val BgCard = Color(0xFFFFFFFF)
+    val BgSubtle = Color(0xFFF4F4F5)
+    val TextPrimary = Color(0xFF0F0E1A)
+    val TextSecondary = Color(0xFF52525B)
+    val TextTertiary = Color(0xFFA1A1AA)
+    val BorderSubtle = Color(0xFFE4E4E7)
+    // endregion
 }
